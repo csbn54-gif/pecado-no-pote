d6d65b2ac8f85a43e5463b48ed8d83f0a8388345
csbn54-gif <csbn54@gmail.com>
2026-08-15 00:27:24 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE = "pecado-no-pote-v1";
+const ASSETS = [
+  "./",
+  "./pecado_no_pote_app.html",
+  "./manifest.webmanifest",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request).catch(() => caches.match("./pecado_no_pote_app.html")))
+  );
+});
